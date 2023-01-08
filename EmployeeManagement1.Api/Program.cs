57c25a97e7b7d7b834ae479e28e4433bc957a2a9
Almer Jansson Pernbert <alpe2011@edu.laroverken.se>
2023-01-08 22:52:41 +0100

    x

--- EmployeeManagement1.Api/Program.cs
+++ EmployeeManagement1.Api/Program.cs
@@ -1,6 +1,25 @@
+using EmployeeManagement.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Net.Security;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// KANSKE INTE FUNGERAR:
+// Problem från denna video (8:50) h ttps://www.youtube.com/watch?v=J7Ho7HS7i_Y&list=PL6n9fhu94yhVowClAs8-6nYnfsOTma14P&index=12 
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(GetConnectionString("DBConnection")));
+
+Action<SqlServerDbContextOptionsBuilder>? GetConnectionString(string v)
+{
+    throw new NotImplementedException();
+}
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
